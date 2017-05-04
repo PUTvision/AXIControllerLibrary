14acd931b91d6f5d7a6c33bdc37d92b9513b3307
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2017-05-04 11:07:22 +0200

    Update submodule

--- userprog/axi_ctrl_test.cpp
+++ userprog/axi_ctrl_test.cpp
@@ -3,6 +3,7 @@
 
 #include <axi_ctrl.hpp>
 #include <libaxidma.h>
+#include <axidma_ioctl.h>
 
 namespace input_registers {
   constexpr const RegisterMapping<0, 0, 0> software_reset;
@@ -28,30 +29,28 @@ void transfer() {
 	if (axidma_dev == NULL) {
 		std::cerr << "Error: Failed to initialize the AXI DMA device." << std::endl;
 		std::exit(1);
-	}
+  }
 
-	int num_tx, num_rx;
+  auto tx_chans = axidma_get_dma_tx(axidma_dev);
+  if (tx_chans->len < 1) {
+    std::cerr << "Error: No transmit channels were found." << std::endl;
+    std::exit(1);
+  }
+  auto rx_chans = axidma_get_dma_rx(axidma_dev);
+  if (rx_chans->len < 1) {
+    std::cerr << "Error: No receive channels were found." << std::endl;
+    std::exit(1);
+  }
 
-	auto tx_chans = axidma_get_dma_tx(axidma_dev, &num_tx);
-	if (num_tx < 1) {
-		std::cerr << "Error: No transmit channels were found." << std::endl;
-		std::exit(1);
-	}
-	auto rx_chans = axidma_get_dma_rx(axidma_dev, &num_rx);
-	if (num_rx < 1) {
-		std::cerr << "Error: No receive channels were found." << std::endl;
-		std::exit(1);
-	}
-
-	std::uint32_t *input = (std::uint32_t *)axidma_malloc(axidma_dev, 64);
-	std::uint32_t *output = (std::uint32_t *)axidma_malloc(axidma_dev, 64);
+  auto input = reinterpret_cast<std::uint32_t *>(axidma_malloc(axidma_dev, 64));
+  auto output = reinterpret_cast<std::uint32_t *>(axidma_malloc(axidma_dev, 64));
 
 	for (std::uint32_t i = 0; i < 16; i++) {
 		input[i] = i;
 	}
 
-	int rc = axidma_twoway_transfer(axidma_dev, tx_chans[0], input, 64,
-																	rx_chans[0], output, 64, true);
+  int rc = axidma_twoway_transfer(axidma_dev, tx_chans->data[0], input, 64,
+                                  rx_chans->data[0], output, 64, true);
 	if (rc < 0) {
 		std::cerr << "DMA read write transaction failed." << std::endl;
 		std::exit(1);
@@ -65,7 +64,7 @@ void transfer() {
 	axidma_free(axidma_dev, output, 64);
 }
 
-int main(int argc, char **argv) {
+int main(int /*argc*/, char */*argv*/[]) {
 		try {
 			AXIController controller{"adder_axi_ctrl"};
 			controller.set_property(input_registers::number_of_coprocessor_operations, 20);
